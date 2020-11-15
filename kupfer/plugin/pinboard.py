0ca9acc29073c7440bfb355cc9445de18064e2ea
Peter Stuifzand <peter@stuifzand.eu>
2020-11-15 23:41:39 +0100

    Create Pinboard bookmarks source

---
+++ kupfer/plugin/pinboard.py
@@ -0,0 +1,42 @@
+__kupfer_name__ = _("Pinboard")
+__kupfer_sources__ = ("PinboardBookmarkSource",)
+__kupfer_actions__ = ()
+__description__ = _("Manage and use bookmarks from Pinboard")
+__version__ = ""
+__author__ = "Peter Stuifzand <peter@p83.nl>"
+
+from kupfer import plugin_support
+from kupfer.objects import Source
+from kupfer.objects import UrlLeaf
+import pinboard
+
+__kupfer_settings__ = plugin_support.PluginSettings(
+    {
+        "key": "token",
+        "label": _("Pinboard API Token"),
+        "type": str,
+        "value": "",
+    },
+)
+
+class PinboardBookmarkSource(Source):
+    def __init__(self):
+        super().__init__(_("Pinboard Bookmarks"))
+        token = __kupfer_settings__["token"]
+        self.pinboard = pinboard.Pinboard(token)
+
+    def get_items(self):
+        bookmarks = self.pinboard.posts.all(start=0, results=100)
+        return [UrlLeaf(b.url, b.description) for b in bookmarks]
+
+    def get_description(self):
+        return _("Index of Pinboard bookmarks")
+
+    def get_gicon(self):
+        return self.get_leaf_repr() and self.get_leaf_repr().get_gicon()
+
+    def get_icon_name(self):
+        return "web-browser"
+
+    def provides(self):
+        yield UrlLeaf
